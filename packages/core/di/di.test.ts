ab40796fae18055ac46c026367b1f880c9a34d3b
Serebro <sergilyin@gmail.com>
2021-01-30 17:06:26 +0200

    fix(core): DI; test

---
+++ packages/core/di/di.test.ts
@@ -0,0 +1,20 @@
+import { DI } from './di'
+
+const get = (source: Function & { prototype: any } | string | symbol): any => {
+    if (typeof source === 'string') {
+        return 1
+    } else if (typeof source === 'symbol') {
+        return 2
+    } else {
+        return 3
+    }
+}
+
+describe('DI', () => {
+    it('should return a service', async () => {
+        DI.init(get)
+        expect(DI.get('string')).toBe(1)
+        expect(DI.get(Symbol('symbol'))).toBe(2)
+        expect(DI.get(() => {})).toBe(3)
+    })
+})
--- packages/core/di/di.ts
+++ packages/core/di/di.ts
@@ -1,14 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class DI {
-    public static init(get: <T>(source: Function & { prototype: T } | string | symbol) => T) {
-        Object.defineProperty(DI, 'get', {
+    public static init(get: <T extends unknown>(source: Function & {prototype: T} | string | symbol) => T) {
+        // @ts-ignore
+        Object.defineProperty(global.__cerioom.DI, 'get', {
             value: get,
             writable: false, // to prevent changes
             configurable: true, // to support destroying properties later
         })
     }
 
-    public static get<T>(source: Function & { prototype: T } | string | symbol): T {
-        throw new Error('Not implemented')
+    public static get<T extends unknown>(source: Function & {prototype: T} | string | symbol): T {
+        // @ts-ignore
+        if (!global.__cerioom?.DI?.get) {
+            throw new Error('Not implemented')
+        }
+
+        // @ts-ignore
+        return global.__cerioom.DI.get(source)
     }
 }
