25cb6302f949059a4d32486ba446e4bde1f6de4c
Serebro <sergilyin@gmail.com>
2021-01-30 17:08:17 +0200

    fix: project files

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,31 @@
 module.exports = {
-  roots: ['./spec'],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['./spec/bootstrap.ts'],
-  verbose: true,
-  globals: {
-    'ts-jest': {
-      tsConfig: './tsconfig.json'
-    }
-  },
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 20,
-  //     functions: 20,
-  //     lines: 20,
-  //     statements: 20
-  //   }
-  // },
-  coverageDirectory: './coverage',
-  coveragePathIgnorePatterns: ['/node_modules/', './dist', './spec']
-}
+    // roots: ['./spec'],
+    preset: 'ts-jest',
+    transform: {
+        '^.+\\.ts$': 'ts-jest'
+    },
+    testMatch: ['<rootDir>/**/*.test.ts'],
+    testEnvironment: 'node',
+    // testRegex: 'test\.ts$',
+    // setupFiles: ['./spec/bootstrap.ts'],
+    verbose: true,
+    globals: {
+        'ts-jest': {
+            tsconfig: './tsconfig.test.json',
+            diagnostics: {
+                ignoreCodes: [2300],
+            },
+        }
+    },
+    // coverageThreshold: {
+    //   global: {
+    //     branches: 20,
+    //     functions: 20,
+    //     lines: 20,
+    //     statements: 20
+    //   }
+    // },
+    coverageDirectory: './coverage',
+    coveragePathIgnorePatterns: ['dist', 'test', '\\.test\\.ts$'],
+    collectCoverageFrom : ['packages/**/*.ts'],
+};
