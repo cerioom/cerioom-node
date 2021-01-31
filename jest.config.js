fd02a8f942200ee144fe6e8c609a20d8b35d8e3d
Serebro <sergilyin@gmail.com>
2021-01-31 14:19:04 +0200

    fix: tsconfig.json

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     verbose: true,
     globals: {
         'ts-jest': {
-            tsconfig: './tsconfig.test.json',
+            tsconfig: './tsconfig.json',
             diagnostics: {
                 ignoreCodes: [2300],
             },
