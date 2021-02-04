3c5551ccec3cb7307a82e7d1d246b170ff1d24fc
Serebro <sergilyin@gmail.com>
2021-02-04 10:01:42 +0200

    fix(currency): small refactoring

--- packages/currency/resource-query/resource-query-field-type.ts
+++ packages/currency/resource-query/resource-query-field-type.ts
@@ -1,10 +1,11 @@
-import { ContextInterface } from '@cerioom/core'
+import { ContextManager } from '@cerioom/core'
 import { DI } from '@cerioom/core'
 import { NumberField, ResourceQueryMapperType } from '@cerioom/resource'
 import { CurrencySerializer } from '../currency-serializer'
 
 
-export const CurrencyField: ResourceQueryMapperType<number> = (value: any, name: string, context: ContextInterface) => {
+export const CurrencyField: ResourceQueryMapperType<number> = (value: any, name: string) => {
+    const context = DI.get(ContextManager).getContext()
     const currencyManager = DI.get(CurrencySerializer).configure({
         currency: context.tenant?.config?.get('currency.alpha3') || 'USD', // todo parameter name
     })
