18a86c23c319028e1ea655aa8f0509d9161ddd33
Serebro <sergilyin@gmail.com>
2021-05-01 12:38:46 +0300

    feat(resource): add array resource query field types

--- packages/resource/resource-query/resource-query-field-type.ts
+++ packages/resource/resource-query/resource-query-field-type.ts
@@ -19,6 +19,11 @@ export const DateField: ResourceQueryMapperType<Date> = (value: any, name: strin
     throw new UnexpectedFieldTypeError().setData({name: name, value: value})
 }
 
+export const DateArrayField: ResourceQueryMapperType<Date[]> = (value: any[], name: string) => {
+    return value.map(v => DateField(v, name))
+}
+
+
 export const EnumField = (expectedEnumType: any) => {
     return (value: any, name: string) => {
         if (value in expectedEnumType) {
@@ -29,9 +34,27 @@ export const EnumField = (expectedEnumType: any) => {
     }
 }
 
+export const EnumArrayField = (expectedEnumType: any) => {
+    return (value: any, name: string) => {
+        return value.map(v => {
+            if (v in expectedEnumType) {
+                return v
+            }
+
+            throw new UnexpectedFieldTypeError().setData({name: name, value: v})
+        })
+    }
+}
+
+
 export const StringField: ResourceQueryMapperType<string> = (value: any, name: string) => {
     return String(value)
 }
+
+export const StringArrayField: ResourceQueryMapperType<string[]> = (value: any[], name: string) => {
+    return value.map(v => StringField(v, name))
+}
+
 
 export const NumberField: ResourceQueryMapperType<number> = (value: any, name: string) => {
     if (typeof value === 'number' || typeof value === 'string') {
@@ -40,6 +63,11 @@ export const NumberField: ResourceQueryMapperType<number> = (value: any, name: s
 
     throw new UnexpectedFieldTypeError().setData({name: name, value: value})
 }
+
+export const NumberArrayField: ResourceQueryMapperType<number[]> = (value: any[], name: string) => {
+    return value.map(v => NumberField(v, name))
+}
+
 
 export const BooleanField: ResourceQueryMapperType<boolean> = (value: any, name: string) => {
     if (
@@ -53,9 +81,18 @@ export const BooleanField: ResourceQueryMapperType<boolean> = (value: any, name:
     throw new UnexpectedFieldTypeError().setData({name: name, value: value})
 }
 
+export const BooleanArrayField: ResourceQueryMapperType<boolean[]> = (value: any[], name: string) => {
+    return value.map(v => BooleanField(v, name))
+}
+
 export type ResourceQueryPropertyType =
     typeof DateField |
+    typeof DateArrayField |
     typeof EnumField |
+    typeof EnumArrayField |
     typeof StringField |
+    typeof StringArrayField |
     typeof NumberField |
-    typeof BooleanField
+    typeof NumberArrayField |
+    typeof BooleanField |
+    typeof BooleanArrayField
