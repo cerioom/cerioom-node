a48dea69fd50bf2527b56e1ae5cd01012047161f
Serebro <sergilyin@gmail.com>
2021-05-20 23:54:01 +0300

    fix(core): validator interface

--- packages/core/validator/validator.interface.ts
+++ packages/core/validator/validator.interface.ts
@@ -1,7 +1,17 @@
+export interface ErrorObject<P = Record<string, any>> {
+    params: P;
+    message?: string;
+    data?: unknown;
+}
+
+export type ValidateFunction<T = unknown> = (this: any, data: any, dataCxt?: any) => data is T
+
 export interface ValidatorInterface {
     setLang(lang: any): this
 
     getLang(): any
+
+    compile(how: any): ValidateFunction
 
     validate(how: any, what: any): any
 }
