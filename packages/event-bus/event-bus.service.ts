a7d88c7af32738f3e19f6ec3d4930f79ea05995c
Serebro <sergilyin@gmail.com>
2021-06-06 00:19:17 +0300

    fix(event-bus): change args to req

--- packages/event-bus/event-bus.service.ts
+++ packages/event-bus/event-bus.service.ts
@@ -1,9 +1,10 @@
-import { RuntimeError, Service } from '@cerioom/core'
+import { RuntimeError, Service, Str } from '@cerioom/core'
 import { EventBusTransportInterface } from './event-bus-transport.interface'
 import { EventBusInterface } from './event-bus.interface'
 
 
 // https://moleculer.services/docs/0.12/transporters.html
+
 export class EventBusService extends Service implements EventBusInterface {
     protected transports: EventBusTransportInterface[] = []
 
@@ -15,7 +16,7 @@ export class EventBusService extends Service implements EventBusInterface {
         }
 
         this.transports = transports
-        this.emit('constructed', this)
+        this.emit('cerioom.event-bus.event-bus-service.constructed', this)
     }
 
     public getOneTransport(kind: string | symbol): EventBusTransportInterface | null {
@@ -27,49 +28,55 @@ export class EventBusService extends Service implements EventBusInterface {
     }
 
     public async publish(event: string | symbol, ...args: any[]): Promise<void> {
+        const req = {kind: '', event: event, messageId: Str.random(), headers: {}, params: {}, query: {}, body: args}
         const promises: any[] = []
         this.transports.forEach(transport => {
-            promises.push(transport.publish(event, args))
+            req.kind = transport.kind
+            promises.push(transport.publish(event, req))
         })
         await Promise.all(promises)
-        this.emit('published', event, args)
+        this.emit('cerioom.event-bus.event-bus-service.published', event, req)
     }
 
     public async send(event: string | symbol, ...args: any): Promise<void> {
+        const req = {kind: '', event: event, messageId: Str.random(), headers: {}, params: {}, query: {}, body: args}
         const promises: any[] = []
         this.transports.forEach(transport => {
-            promises.push(transport.send(event, ...args))
+            req.kind = transport.kind
+            promises.push(transport.send(event, req))
         })
         await Promise.all(promises)
-        this.emit('send', event, args)
+        this.emit('cerioom.event-bus.event-bus-service.sent', event, req)
     }
 
     public async request(event: string | symbol, ...args: any[]): Promise<any[]> {
+        const req = {kind: '', event: event, messageId: Str.random(), headers: {}, params: {}, query: {}, body: args}
         const promises: any[] = []
         this.transports.forEach(transport => {
-            promises.push(transport.request(event, args))
+            req.kind = transport.kind
+            promises.push(transport.request(event, req))
         })
 
         const results = await Promise.all(promises)
-        this.emit('requested', event, args, results)
+        this.emit('cerioom.event-bus.event-bus-service.requested', event, req, results)
         return results
     }
 
-    public async unsubscribe(event: string | symbol, listener: (...args: any[]) => void): Promise<void> {
+    public async unsubscribe(event: string | symbol, listener: (req: any) => void): Promise<void> {
         const promises: any[] = []
         this.transports.forEach(transport => {
             promises.push(transport.unsubscribe(event, listener))
         })
         await Promise.all(promises)
-        this.emit('unsubscribe', event, listener)
+        this.emit('cerioom.event-bus.event-bus-service.unsubscribed', event, listener)
     }
 
-    public async subscribe(event: string | symbol, listener: (...args: any[]) => void): Promise<void> {
+    public async subscribe(event: string | symbol, listener: (req: any) => void): Promise<void> {
         const promises: any[] = []
         this.transports.forEach(transport => {
             promises.push(transport.subscribe(event, listener))
         })
         await Promise.all(promises)
-        this.emit('subscribe', event, listener)
+        this.emit('cerioom.event-bus.event-bus-service.subscribed', event, listener)
     }
 }
