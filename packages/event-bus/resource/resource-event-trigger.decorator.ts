c8f6809d092aac297ad58dabf3e4d439aa54336a
Serebro <sergilyin@gmail.com>
2021-06-11 13:39:03 +0300

    fix(event-bus): resource-event-trigger decorator

--- packages/event-bus/resource/resource-event-trigger.decorator.ts
+++ packages/event-bus/resource/resource-event-trigger.decorator.ts
@@ -1,4 +1,4 @@
-import { DI, RuntimeError, Str } from '@cerioom/core'
+import { DI, RuntimeError, Security, Str } from '@cerioom/core'
 import { EventBusService } from '../event-bus.service'
 import 'reflect-metadata'
 
@@ -12,6 +12,21 @@ export function ResourceEventTrigger () {
         const originalMethod = descriptor.value
         descriptor.value = async function (...args: any[]): Promise<any> {
             const eventBus = DI.get(EventBusService)
+            const format = Reflect.getMetadata('resourceEvent:format', this.constructor)
+            const resource = Reflect.getMetadata('resourceEvent:resourceName', this.constructor)
+            const action = Reflect.getMetadata('resourceEvent:actionName', target.constructor, propertyKey)
+            const maskFields = Reflect.getMetadata('security:maskFields', target.constructor, propertyKey)
+            const event = Str.resolveTemplate(format, {resource: resource, action: action})
+
+            let eventPayload = {data: args}
+            if (maskFields?.length) {
+                eventPayload = {
+                    data: Array.isArray(args)
+                        ? args.map(el => Security.maskFields(el, maskFields))
+                        : Security.maskFields(args, maskFields)
+                }
+            }
+
             try {
                 let result = originalMethod.apply(this, args)
                 if (result instanceof Promise) {
@@ -19,11 +34,7 @@ export function ResourceEventTrigger () {
                 }
 
                 if (eventBus && 'publish' in eventBus) {
-                    const format = Reflect.getMetadata('resourceEvent:format', this.constructor)
-                    const resource = Reflect.getMetadata('resourceEvent:resourceName', this.constructor)
-                    const action = Reflect.getMetadata('resourceEvent:actionName', target.constructor, propertyKey)
-                    const event = Str.resolveTemplate(format, {resource: resource, action: action})
-                    setImmediate(async () => await eventBus.publish(event, {data: args}))
+                    setImmediate(async () => await eventBus.publish(event, eventPayload))
                 } else {
                     // todo logging
                 }
@@ -32,7 +43,7 @@ export function ResourceEventTrigger () {
                 this.log.error({error: RuntimeError.toLog(err)})
                 if (eventBus && 'publish' in eventBus) {
                     // @ts-expect-error
-                    setImmediate(async () => await eventBus.publish(event, {data: args, error: RuntimeError.toJSON(err)}))
+                    setImmediate(async () => await eventBus.publish(event, {data: eventPayload, error: RuntimeError.toJSON(err)}))
                 } else {
                     // todo logging
                 }
