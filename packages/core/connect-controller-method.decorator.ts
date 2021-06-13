3d9628fbf8c6cbd8fce42abc37946a0c5c96f4de
Serebro <sergilyin@gmail.com>
2021-06-14 01:32:06 +0300

    fix(core): add @Session, @Ip, @HostParam to connect-controller-method

--- packages/core/connect-controller-method.decorator.ts
+++ packages/core/connect-controller-method.decorator.ts
@@ -7,28 +7,35 @@ const RES = 1
 const NEXT = 2
 
 
-export function ConnectControllerMethod (): MethodDecorator {
+export function ConnectControllerMethod(): MethodDecorator {
     // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
     return <T>(target: Object, methodName: string | symbol, descriptor: TypedPropertyDescriptor<T>): TypedPropertyDescriptor<T> | void => {
         const originalMethod = descriptor.value
 
         // @ts-expect-error
-        descriptor.value = async function (...args: any[]): any {
+        descriptor.value = async function(...args: any[]): any {
             try {
                 const data = <any[]> Reflect.getMetadata('connectController:param', target, methodName) || []
                 const newArgs = data
                     .sort((a, b) => a.argIndex > b.argIndex ? 1 : -1)
                     .map(conf => {
                         switch (conf.type) {
+                            case 'session':
                             case 'headers':
                             case 'params':
                             case 'query':
                             case 'body':
+                            case 'ip':
+                            case 'hosts':
                                 return conf.name ? _.get(args[REQ][conf.type], conf.name) : args[REQ][conf.type]
-                            case 'request': return args[REQ]
-                            case 'response': return args[RES]
-                            case 'next': return args[NEXT]
-                            default: return null
+                            case 'request':
+                                return args[REQ]
+                            case 'response':
+                                return args[RES]
+                            case 'next':
+                                return args[NEXT]
+                            default:
+                                return null
                         }
                     })
                     .filter(Boolean)
@@ -50,7 +57,7 @@ export function ConnectControllerMethod (): MethodDecorator {
     }
 }
 
-export function Headers (name?: string) {
+export function Headers(name?: string) {
     return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
         const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
         data.push({type: 'headers', argIndex: parameterIndex, name: name})
@@ -58,7 +65,7 @@ export function Headers (name?: string) {
     }
 }
 
-export function Params (name?: string) {
+export function Param(name?: string) {
     return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
         const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
         data.push({type: 'params', argIndex: parameterIndex, name: name})
@@ -66,7 +73,7 @@ export function Params (name?: string) {
     }
 }
 
-export function Query (name?: string) {
+export function Query(name?: string) {
     return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
         const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
         data.push({type: 'query', argIndex: parameterIndex, name: name})
@@ -74,7 +81,7 @@ export function Query (name?: string) {
     }
 }
 
-export function Body (name?: string) {
+export function Body(name?: string) {
     return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
         const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
         data.push({type: 'body', argIndex: parameterIndex, name: name})
@@ -82,38 +89,50 @@ export function Body (name?: string) {
     }
 }
 
-export function Request () {
+export function Request() {
     return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
         const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
-        data.push({
-            type: 'request',
-            argIndex: parameterIndex,
-        })
-
+        data.push({type: 'request', argIndex: parameterIndex})
         Reflect.defineMetadata('connectController:param', data, target, propertyKey)
     }
 }
 
-export function Response () {
+export function Response() {
     return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
         const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
-        data.push({
-            type: 'response',
-            argIndex: parameterIndex,
-        })
-
+        data.push({type: 'response', argIndex: parameterIndex})
         Reflect.defineMetadata('connectController:param', data, target, propertyKey)
     }
 }
 
-export function Next () {
+export function Next() {
     return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
         const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
-        data.push({
-            type: 'next',
-            argIndex: parameterIndex,
-        })
+        data.push({type: 'next', argIndex: parameterIndex})
+        Reflect.defineMetadata('connectController:param', data, target, propertyKey)
+    }
+}
 
+export function Session() {
+    return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
+        const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
+        data.push({type: 'session', argIndex: parameterIndex})
+        Reflect.defineMetadata('connectController:param', data, target, propertyKey)
+    }
+}
+
+export function Ip() {
+    return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
+        const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
+        data.push({type: 'ip', argIndex: parameterIndex})
+        Reflect.defineMetadata('connectController:param', data, target, propertyKey)
+    }
+}
+
+export function HostParam(name?: string) {
+    return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
+        const data = Reflect.getMetadata('connectController:param', target, propertyKey) || []
+        data.push({type: 'hosts', argIndex: parameterIndex, name: name})
         Reflect.defineMetadata('connectController:param', data, target, propertyKey)
     }
 }
